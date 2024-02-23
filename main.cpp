a27abc6b24ecb5ce4322b7a801931bafe63bacc7
ReemOsama30 <reem.osama3052001@gmail.com>
2024-02-23 12:41:28 +0200

    fix logical errors

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include "Bank.h"
 #include <iostream>
 #include <fstream>
-#include <iostream>
 using namespace std;
-int main()
-{
+
+int main() {
     ofstream outputFile("myfile.txt", ios::app);
-    ifstream inputFile("input.txt");
-  
+    ifstream inputFile("rush.txt");
+
     if (!inputFile) {
         cerr << "Failed to open the input file." << endl;
         return 1;
@@ -16,22 +15,19 @@ int main()
     cout << "___________________________Bank Management system__________________________\n";
     bank b(3);
     int num;
-    inputFile >> num; 
-    for (int i = 0; i < num; i++)
-    {
+    inputFile >> num;
+    inputFile.ignore(); // Ignore the newline character after num
+    for (int i = 0; i < num; i++) {
         int id, arrivaltime, servicetime;
         inputFile >> id >> arrivaltime >> servicetime; // Read customer details from the file
         b.addCustomers(id, servicetime, arrivaltime);
     }
     inputFile.close();
     b.assignCustomers();
-    
 
-
-    //print the final report 
+    // Print the final report 
     b.printFinalReport(outputFile);
     b.printFinalReportToConsole();
-   
 
+    return 0;
 }
-
