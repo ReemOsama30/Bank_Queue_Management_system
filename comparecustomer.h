10017064832e236bfeef1bc57fa48bce870050a5
ReemOsama30 <reem.osama3052001@gmail.com>
2024-02-23 20:00:11 +0200

    remove old files

--- comparecustomer.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "customer.h"
-struct CompareCustomer {
-    bool operator()(const customer& c1, const customer& c2) {
-        return c1.getArrivalTime() > c2.getArrivalTime(); 
-     }
-};
